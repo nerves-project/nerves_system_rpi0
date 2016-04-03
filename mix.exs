5a9bf55a8ada28f1fb8ba284490602ac307340ae
Justin Schneck <jschneck@mac.com>
2016-04-02 22:09:07 -0400

    updated license and package info for hex

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule NervesSystemRpi.Mixfile do
      version: "0.4.0-rc2",
      elixir: "~> 1.2",
      compilers: Mix.compilers ++ [:nerves_system],
+     description: description,
+     package: package,
      deps: deps]
   end
 
@@ -14,11 +16,21 @@ defmodule NervesSystemRpi.Mixfile do
   end
 
   defp deps do
-    [
-      {:nerves_system, path: "../nerves_system"},
-      {:nerves_system_br, path: "../nerves-system-br"},
-      {:nerves_toolchain_arm_unknown_linux_gnueabihf, github: "nerves-project/nerves_toolchain_arm_unknown_linux_gnueabi"}
-    ]
+    [{:nerves_system, path: "../nerves_system"},
+     {:nerves_system_br, path: "../nerves-system-br"},
+     {:nerves_toolchain_arm_unknown_linux_gnueabihf, github: "nerves-project/nerves_toolchain_arm_unknown_linux_gnueabi"}]
+  end
+
+  defp description do
+    """
+    Nerves System - Raspberry Pi A+ / B+ / B / Zero
+    """
+  end
+
+  defp package do
+    [maintainers: ["Frank Hunleth", "Justin Schneck"],
+     licenses: ["Apache 2.0"],
+     links: %{"Github" => "https://github.com/nerves-project/nerves_system_rpi"}]
   end
 
 end
