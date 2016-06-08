a581893080b0e90c70a383d3f3d0ded8cdd38dfd
Justin Schneck <jschneck@mac.com>
2016-06-08 14:45:28 -0400

    point to new bucket

--- nerves.exs
+++ nerves.exs
@@ -9,7 +9,7 @@ config :nerves_system_rpi, :nerves_env,
   type: :system,
   mirrors: [
     "https://github.com/nerves-project/nerves_system_rpi/releases/download/v#{version}/nerves_system_rpi-v#{version}.tar.gz",
-    "https://s3.amazonaws.com/nerves-releases/nerves_pkg/nerves_system_rpi-#{version}.tar.gz"],
+    "https://s3.amazonaws.com/nerves/artifacts/nerves_system_rpi-#{version}.tar.gz"],
   build_platform: Nerves.System.Platforms.BR,
   build_config: [
     defconfig: "nerves_defconfig"
