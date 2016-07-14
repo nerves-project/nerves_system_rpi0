2ea742653b7dabf7b57d82f0abb27e754782b093
Frank Hunleth <fhunleth@troodon-software.com>
2016-07-13 20:31:30 -0400

    Bump nerves_system_br

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule NervesSystemRpi.Mixfile do
 
   defp deps do
     [{:nerves_system, "~> 0.1.4"},
-     {:nerves_system_br, github: "nerves-project/nerves_system_br", ref: "fcf6eb3215913fdbc2716590e245b720775ccad2"},
+     {:nerves_system_br, github: "nerves-project/nerves_system_br", ref: "edc6a51c4254b26161e1d3b40765739a61e1980d"},
      #{:nerves_system_br, "~> 0.6.0"},
      {:nerves_toolchain_armv6_rpi_linux_gnueabi, "~> 0.6.1"}]
   end
