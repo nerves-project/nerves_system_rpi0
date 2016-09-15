b257ffe07a82720242dfe918b075b2de4d81b889
Justin Schneck <jschneck@mac.com>
2016-09-14 20:32:55 -0400

    bump version and add 0.7.1 toolchain

--- mix.exs
+++ mix.exs
@@ -23,7 +23,14 @@ defmodule NervesSystemRpi.Mixfile do
     [{:nerves_system, "~> 0.1.4"},
       #{:nerves_system_br, "~> 0.6.1"},
      {:nerves_system_br, github: "nerves-project/nerves_system_br", tag: "26145be1d313387cdd3cefb7c233fedde806e727"},
-     {:nerves_toolchain_armv6_rpi_linux_gnueabi, "~> 0.6.1"}]
+     {:nerves_toolchain_armv6_rpi_linux_gnueabi,
+        github: "nerves-project/toolchains",
+        sparse: "nerves_toolchain_armv6_rpi_linux_gnueabi"},
+     {:nerves_toolchain_ctng,
+        github: "nerves-project/toolchains",
+        sparse: "nerves_toolchain_ctng",
+        override: true}
+    ]
   end
 
   defp description do
