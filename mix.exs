b4cb7943b798380cdf395b8863b719c9082bc3b0
Justin Schneck <jschneck@mac.com>
2016-09-18 13:15:52 -0400

    Deploy artifacts to S3

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule NervesSystemRpi.Mixfile do
   end
 
   defp deps do
-    [{:nerves_system, "~> 0.1.4"},
+    [{:nerves_system, github: "nerves-project/nerves_system"},
       #{:nerves_system_br, "~> 0.6.1"},
      {:nerves_system_br, github: "nerves-project/nerves_system_br", tag: "26145be1d313387cdd3cefb7c233fedde806e727"},
      {:nerves_toolchain_armv6_rpi_linux_gnueabi,
