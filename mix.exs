53ced962d5d3122b60016da1437f3ac7f7c25db5
Justin Schneck <jschneck@mac.com>
2017-01-26 10:27:07 -0500

    update nerves_system_br to 0.9.0

--- mix.exs
+++ mix.exs
@@ -22,8 +22,8 @@ defmodule NervesSystemRpi.Mixfile do
 
   defp deps do
     [{:nerves, "~> 0.4.0"},
-      #{:nerves_system_br, "~> 0.8.1"},
-     {:nerves_system_br, github: "nerves-project/nerves_system_br", branch: "br-2016.11.1-bump"},
+     {:nerves_system_br, "~> 0.9.0"},
+     #{:nerves_system_br, github: "nerves-project/nerves_system_br", branch: "br-2016.11.1-bump"},
      {:nerves_toolchain_armv6_rpi_linux_gnueabi, "~> 0.9.0"}]
   end
 
