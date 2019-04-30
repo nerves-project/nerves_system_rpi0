8b8d2de6a5ebdf524d603792a53dc4ab3fbc8f8d
Frank Hunleth <fhunleth@troodon-software.com>
2019-04-30 07:56:55 -0400

    nerves_system_br: bump to v1.7.2

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule NervesSystemRpi0.MixProject do
   defp deps do
     [
       {:nerves, "~> 1.3", runtime: false},
-      {:nerves_system_br, "1.7.1", runtime: false},
+      {:nerves_system_br, "1.7.2", runtime: false},
       {:nerves_toolchain_armv6_rpi_linux_gnueabi, "1.1.0", runtime: false},
       {:nerves_system_linter, "~> 0.3.0", runtime: false},
       {:ex_doc, "~> 0.18", only: [:dev, :test], runtime: false}
