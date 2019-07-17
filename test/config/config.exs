8ae9c7edb1ba1c121a367170587eec7cb953513c
Justin Schneck <jschneck@mac.com>
2019-07-17 10:23:20 -0400

    Update system-test firmware

--- test/config/config.exs
+++ test/config/config.exs
@@ -2,12 +2,10 @@ use Mix.Config
 
 # Repository specific configuration
 
-system = :nerves_system_rpi0
 platform = "rpi0"
 arch = "arm"
 
 app_part_devparth = "/dev/mmcblk0p3"
-network_interface = System.get_env("NERVES_NETWORK_INTERFACE") || "wlan0"
 
 # Environment specific configuration
 #  Nerves Project test farm configuration
@@ -22,58 +20,26 @@ websocket_protocol = System.get_env("WEBSOCKET_PROTOCOL") || "ws"
 # Configure shoehorn boot order.
 config :shoehorn,
   app: :nerves_system_test,
-  init: [:nerves_runtime, :system_registry_term_storage, :nerves_network]
+  init: [:nerves_runtime, :vintage_net]
 
-# Only trust signed firmware
-config :nerves_system_test, :firmware, public_key: System.get_env("NERVES_FW_PUB_KEY")
-
-# Configure system_registry term storage to store the wifi credentials on the
-#  app data partition. If the device is using eth0 as the primary connection
-#  mechanism the wlan0 settings do not need to be configured.
-config :system_registry, SystemRegistry.TermStorage,
-  path: "/root/system_registry",
-  scopes: [
-    [:config, :network_interface, "wlan0", :ssid],
-    [:config, :network_interface, "wlan0", :psk]
+config :nerves_hub,
+  public_keys: [
+    System.get_env("NERVES_HUB_FW_PUBLIC_KEY")
   ]
 
-# Configure the default interface settings.
-# wlan0 | eth0 - Used to establish a connection to the test server.
-# usb0 - configured with linklocal to be validated as part of the test results.
-config :nerves_network, :default,
-  eth0: [
-    ipv4_address_method: :dhcp
-  ],
-  wlan0: [
-    ipv4_address_method: :dhcp
-  ],
-  usb0: [
-    ipv4_address_method: :linklocal
+config :vintage_net,
+  regulatory_domain: "US",
+  config: [
+    {"eth0", %{type: VintageNet.Technology.Ethernet, ipv4: %{method: :dhcp}}},
+    {"wlan0", %{type: VintageNet.Technology.WiFi}}
   ]
 
 # Configure the url for the connection to the test server phoenix channel socket.
-config :nerves_system_test, NervesTestServer.Socket,
+config :nerves_test_client, :socket,
   url: "#{websocket_protocol}://#{test_server}/socket/websocket"
 
-# Configure the test suite. nerves_system_test needs to know information such as
-#  system - the name of the system repo the tests are being executed on
-#  network_interface - the interface that should be used for reporting the
-#   results on
-#  tests - the locations for the tests to run.
-#    Currently, it is only supported that tests are included in the priv_dir
-#    of the app and dependencies. The default layout runs tests that are common
-#    across devices (:nerves_system_test) and those that are specific to the
-#    device (this app)
-config :nerves_system_test,
-  system: system,
-  network_interface: network_interface,
-  tests: [
-    {:test, :priv_dir, "test"},
-    {:nerves_system_test, :priv_dir, "test"}
-  ]
-
 # The configuration stored here is duplicated from the project so it can be
-#  validated by nerves_system_test because the source is unavailable at runtime.
+#  validated by nerves_test_client because the source is unavailable at runtime.
 config :nerves_runtime, :kv,
   nerves_fw_application_part0_devpath: app_part_devparth,
   nerves_fw_application_part0_fstype: "ext4",
