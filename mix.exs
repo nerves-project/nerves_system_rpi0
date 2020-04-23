00d2a619ce278508f7377334ad3a1dcdcc424085
Justin Schneck <jschneck@mac.com>
2020-04-22 21:50:42 -0400

    toolchain: bump to v1.3.2

--- mix.exs
+++ mix.exs
@@ -51,6 +51,7 @@ defmodule NervesSystemRpi0.MixProject do
     [
       {:nerves, "~> 1.5.4 or ~> 1.6.0", runtime: false},
       {:nerves_system_br, "1.11.2", runtime: false},
+      {:nerves_toolchain_armv6_rpi_linux_gnueabi, "~> 1.3.0", runtime: false},
       {:nerves_system_linter, "~> 0.3.0", runtime: false},
       {:ex_doc, "~> 0.18", only: [:dev, :test], runtime: false}
     ]
