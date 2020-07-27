ead278092508039255ec5a0703fdd42fbe48ace7
Jon Carstens <jjcarstens@me.com>
2020-07-27 15:37:58 -0600

    bump ex_doc 0.22.2 and :docs env changes
    * Adds CHANGELOG.md to docs
    * sets `preferred_cli_env` for ex_doc so it's not compiled in tests

--- mix.exs
+++ mix.exs
@@ -3,6 +3,7 @@ defmodule NervesSystemRpi0.MixProject do
 
   @github_organization "nerves-project"
   @app :nerves_system_rpi0
+  @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
            |> String.trim()
@@ -18,7 +19,12 @@ defmodule NervesSystemRpi0.MixProject do
       package: package(),
       deps: deps(),
       aliases: [loadconfig: [&bootstrap/1], docs: ["docs", &copy_images/1]],
-      docs: [extras: ["README.md"], main: "readme"]
+      docs: docs(),
+      preferred_cli_env: %{
+        docs: :docs,
+        "hex.build": :docs,
+        "hex.publish": :docs
+      }
     ]
   end
 
@@ -53,7 +59,7 @@ defmodule NervesSystemRpi0.MixProject do
       {:nerves_system_br, "1.12.2", runtime: false},
       {:nerves_toolchain_armv6_rpi_linux_gnueabi, "~> 1.3.0", runtime: false},
       {:nerves_system_linter, "~> 0.4", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.18", only: [:dev, :test], runtime: false}
+      {:ex_doc, "~> 0.22", only: :docs, runtime: false}
     ]
   end
 
@@ -63,11 +69,21 @@ defmodule NervesSystemRpi0.MixProject do
     """
   end
 
+  defp docs do
+    [
+      extras: ["README.md", "CHANGELOG.md"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url,
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
+    ]
+  end
+
   defp package do
     [
       files: package_files(),
       licenses: ["Apache 2.0"],
-      links: %{"GitHub" => "https://github.com/#{@github_organization}/#{@app}"}
+      links: %{"GitHub" => @source_url}
     ]
   end
 
