f9d0bb1c469faa75e50b7b2e935cd0500c8c7b47
Frank Hunleth <fhunleth@troodon-software.com>
2022-05-12 22:52:11 -0400

    linux: bump to 5.15.32
    This enables `CONFIG_NOP_USB_XCEIV=y` to fix a change between Linux 5.10
    and Linux 5.15 that broke USB gadget mode. See
    https://github.com/raspberrypi/linux/issues/4496#issuecomment-891087565
    for more information.
    
    The other Linux configuration changes are due to a rearrangement of
    options or the change of option for the PoE hat.

--- mix.exs
+++ mix.exs
@@ -109,7 +109,7 @@ defmodule NervesSystemRpi0.MixProject do
       "fwup-revert.conf",
       "fwup.conf",
       "LICENSE",
-      "linux-5.10.defconfig",
+      "linux-5.15.defconfig",
       "mix.exs",
       "nerves_defconfig",
       "post-build.sh",
