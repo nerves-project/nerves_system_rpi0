aa6ca38aef47bf22996a7a64daae2adc5c648689
Frank Hunleth <fhunleth@troodon-software.com>
2023-11-11 13:17:59 -0500

    Bump kernel to stable_20231123 (6.1.63)

--- mix.exs
+++ mix.exs
@@ -109,7 +109,7 @@ defmodule NervesSystemRpi0.MixProject do
       "fwup-ops.conf",
       "fwup.conf",
       "LICENSE",
-      "linux-5.15.defconfig",
+      "linux-6.1.defconfig",
       "mix.exs",
       "nerves_defconfig",
       "post-build.sh",
