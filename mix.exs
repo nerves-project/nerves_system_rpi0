47acddaa32e7fbaba7d752618c61893ecaec10da
Frank Hunleth <fhunleth@troodon-software.com>
2025-12-07 21:33:50 -0500

    Reduce fwup redundancy by using an EEx template
    The final fwup.conf file is still being committed to allow time to grow
    trust in using a template language to generate the fwup.conf.
    
    This refactor is really needed for TRYBOOT support due to significantly
    more fwup.conf redundancy to migrate pre-TRYBOOT devices.

--- mix.exs
+++ mix.exs
@@ -19,7 +19,10 @@ defmodule NervesSystemRpi0.MixProject do
       description: description(),
       package: package(),
       deps: deps(),
-      aliases: [loadconfig: [&bootstrap/1]],
+      aliases: [
+        loadconfig: [&bootstrap/1],
+        generate_fwup_conf: &generate_fwup_conf/1
+      ],
       docs: docs()
     ]
   end
@@ -111,6 +114,7 @@ defmodule NervesSystemRpi0.MixProject do
       "cmdline.txt",
       "config.txt",
       "fwup-ops.conf",
+      "fwup.conf.eex",
       "fwup.conf",
       "LICENSES/*",
       "linux-6.12.defconfig",
@@ -143,5 +147,16 @@ defmodule NervesSystemRpi0.MixProject do
     else
       System.put_env("MIX_TARGET", "target")
     end
+  end
+
+  defp generate_fwup_conf(_args) do
+    template_path = Path.join(__DIR__, "fwup.conf.eex")
+    output_path = Path.join(__DIR__, "fwup.conf")
+
+    Mix.shell().info("Generating fwup.conf")
+
+    content = EEx.eval_file(template_path)
+    File.write!(output_path, content)
+    Mix.shell().info("Successfully generated #{output_path}")
   end
 end
